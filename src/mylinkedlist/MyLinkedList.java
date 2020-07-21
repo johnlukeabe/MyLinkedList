ee81084aff5e43fda97daeba19f0eadbc1bd1476
Abraham Sohn <aso@skyplan.com>
2020-07-20 20:11:29 -0600

    Added new files for mylinkedlist
    Linkedlist JAVA eclipse proejct files

---
+++ src/mylinkedlist/MyLinkedList.java
@@ -0,0 +1,31 @@
+package mylinkedlist;
+
+import java.util.LinkedList;
+
+public class MyLinkedList {
+	public static void main(String[] args) {
+		LinkedList<String> myll = new LinkedList<String>();
+		
+		System.out.println("size:" + myll.size());
+		myll.add("Akali");
+		System.out.println(myll);
+		myll.add(1, "Brand");
+		System.out.println(myll);
+		myll.add(0, "Cho'Gath");
+		System.out.println(myll);
+		myll.addFirst("Darius");
+		System.out.println(myll);
+		myll.addLast("Ezreal");
+		System.out.println(myll);
+		System.out.println("size:" + myll.size());
+		System.out.println(myll.getFirst());
+		System.out.println(myll.getLast());
+		System.out.println(myll.get(2));
+		myll.removeFirst();		
+		System.out.println(myll);
+		myll.removeLast();
+		System.out.println(myll);
+		myll.remove(1);
+		System.out.println(myll);
+	}
+}
